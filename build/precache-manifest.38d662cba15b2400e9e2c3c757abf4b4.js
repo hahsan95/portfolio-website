f08e93c7bf003242bf86c407f912b811d80508e9
Ahsan, Homum <hahsan@conversantmedia.com>
2020-05-28 22:31:09 -0500

    added github pages

---
+++ build/precache-manifest.38d662cba15b2400e9e2c3c757abf4b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4685f894f1984c19a036b28b46eaf85",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "7d931b1166311e37162f",
+    "url": "/portfolio-website/static/css/main.e4d219b9.chunk.css"
+  },
+  {
+    "revision": "51dfa01edfafcb4319bc",
+    "url": "/portfolio-website/static/js/2.1c8c47e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-website/static/js/2.1c8c47e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d931b1166311e37162f",
+    "url": "/portfolio-website/static/js/main.5ac72814.chunk.js"
+  },
+  {
+    "revision": "1cffdc391d7c9f779849",
+    "url": "/portfolio-website/static/js/runtime-main.f538cae4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
